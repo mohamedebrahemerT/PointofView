a5bb65bfebc31b6b2c08454364aa2bde81add3ed
mohamedebrahemerT <mohamedchi2013@gmail.com>
2022-04-06 23:17:51 +0200

    up
    up

--- resources/views/Forentend/layouts/header.blade.php
+++ resources/views/Forentend/layouts/header.blade.php
@@ -6,6 +6,19 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <title>   {{Settings()->name}}  </title>
+
+<link rel="shortcut icon" href="{{url('/')}}/Forentend/iconified/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="{{url('/')}}/Forentend/iconified/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{url('/')}}/Forentend/iconified/apple-touch-icon-180x180.png" />
+
+
   <!-- Bootstrap core CSS -->
   <link href="{{url('/')}}/Forentend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Fontawesome CSS -->
